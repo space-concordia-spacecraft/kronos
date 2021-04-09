b92813906cc2ec5eb66005202c2e53de5cca3aa4
Cristian Milatinov <milatinovcristian@gmail.com>
2021-04-09 18:36:21 -0400

    add gitignore + static cast for keyhash

--- Kronos_lib/include/types/keyhash.h
+++ Kronos_lib/include/types/keyhash.h
@@ -10,7 +10,7 @@
 template <typename K, size_t tableSize>
 struct KeyHash {
     unsigned long operator()(const K &key) const {
-        return reinterpret_cast<unsigned long>(key) % tableSize;
+        return static_cast<unsigned long>(key) % tableSize;
     }
 };
 #endif //KRONOS_KEYHASH_H
