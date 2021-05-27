8087ae4d3923d279baf3f984fcb008dce2d750e8
Jason Kim <jason.k99@hotmail.com>
2021-05-26 21:27:40 -0400

    Added VectorTesting

---
+++ Google_tests/VectorTests.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "ks_vector.h"
+
+class VectorFixture : public ::testing::Test {
+
+protected:
+    kronos::Vector<int>* vector;
+
+    virtual void SetUp() {
+        vector = new kronos::Vector<int>();
+    }
+
+    virtual void TearDown() {
+        delete vector;
+    }
+
+};
+
+
+TEST_F(VectorFixture, PutAndGetSuccessScenario){
+    int val;
+    vector->add(10);
+
+    EXPECT_TRUE(vector->contains(10));
+    EXPECT_EQ(val, 10);
+
+//    EXPECT_TRUE(vector->remove(10));
+}
+//
+TEST_F(VectorFixture, PutAndGetFailingScenario){
+//    EXPECT_FALSE(vector->contains(10));
+}
