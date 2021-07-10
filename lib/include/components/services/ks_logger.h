9751271dfd4e10471daad7dfdc838832b59f59a5
Cristian Milatinov <milatinovcristian@gmail.com>
2021-07-09 22:29:36 -0400

    more fixes

--- lib/include/components/services/ks_logger.h
+++ lib/include/components/services/ks_logger.h
@@ -12,21 +12,20 @@ namespace kronos {
         uint8_t severity;
     };
 
-
-
     class ComponentLogger : public ComponentQueued {
     public:
         ComponentLogger(const String &name, const String &filepath);
 
         KsCmdResult ProcessCommand(const CommandMessage& message) override;
+
+        void Init() override;
+
+    private:
+        String m_FilePath;
+
         void ClearLogs();
         void ChangeFilepath(const String &str);
-
         String ConvertTimestamp(uint32_t timestamp);
-        void Init() override;
-    private:
-
-        String m_FilePath;
     };
 
 }
