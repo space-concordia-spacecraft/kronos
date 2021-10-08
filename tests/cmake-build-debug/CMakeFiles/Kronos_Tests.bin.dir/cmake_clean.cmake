00bd6c164f7f3d6f87de6a02b37ebef5bd3d0e69
Ruben <ruben.e.sanchez.a@gmail.com>
2021-10-08 00:16:46 -0400

    removed cmake build

--- tests/cmake-build-debug/CMakeFiles/Kronos_Tests.bin.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Kronos_Tests.bin"
-  "Kronos_Tests.map"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Kronos_Tests.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- tests/cmake-build-debug/CMakeFiles/Kronos_Tests.bin.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Kronos_Tests.bin.
-# This may be replaced when dependencies are built.
