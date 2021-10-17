623a55295233213c95f802f0ad783d710d6cd035
redknight990 <milatinovcristian@gmail.com>
2021-10-17 02:50:25 -0400

    commented out the server and client, must refactor so that a new csp task is created

--- lib/src/components/drivers/ks_csp.cpp
+++ lib/src/components/drivers/ks_csp.cpp
@@ -5,116 +5,6 @@ namespace kronos {
 
     }
 
-    /* Server task - handles requests from clients */
-    CSP_DEFINE_TASK(task_server) {
-
-        csp_log_info("Server task started");
-
-        /* Create socket with no specific socket options, e.g. accepts CRC32, HMAC, XTEA, etc. if enabled during compilation */
-        csp_socket_t *sock = csp_socket(CSP_SO_NONE);
-
-        /* Bind socket to all ports, e.g. all incoming connections will be handled here */
-        csp_bind(sock, CSP_ANY);
-
-        /* Create a backlog of 10 connections, i.e. up to 10 new connections can be queued */
-        csp_listen(sock, 10);
-
-        /* Wait for connections and then process packets on the connection */
-        while (1) {
-
-            /* Wait for a new connection, 10000 mS timeout */
-            csp_conn_t *conn;
-            if ((conn = csp_accept(sock, 10000)) == NULL) {
-                /* timeout */
-                continue;
-            }
-
-            /* Read packets on connection, timout is 100 mS */
-            csp_packet_t *packet;
-            while ((packet = csp_read(conn, 50)) != NULL) {
-                switch (csp_conn_dport(conn)) {
-                    case MY_SERVER_PORT:
-                        /* Process packet here */
-                    csp_log_info("Packet received on MY_SERVER_PORT: %s", (char *) packet->data);
-                        csp_buffer_free(packet);
-                        ++server_received;
-                        break;
-
-                    default:
-                        /* Call the default CSP service handler, handle pings, buffer use, etc. */
-                        csp_service_handler(conn, packet);
-                        break;
-                }
-            }
-
-            /* Close current connection */
-            csp_close(conn);
-
-        }
-
-        return CSP_TASK_RETURN;
-
-    }
-/* End of server task */
-
-/* Client task sending requests to server task */
-    CSP_DEFINE_TASK(task_client) {
-
-        csp_log_info("Client task started");
-
-        unsigned int count = 0;
-
-        while (1) {
-
-            csp_sleep_ms(test_mode ? 200 : 1000);
-
-            /* Send ping to server, timeout 1000 mS, ping size 100 bytes */
-            int result = csp_ping(m_ServerAddress, 1000, 100, CSP_O_NONE);
-            csp_log_info("Ping address: %u, result %d [mS]", m_ServerAddress, result);
-
-            /* Send reboot request to server, the server has no actual implementation of csp_sys_reboot() and fails to reboot */
-            csp_reboot(m_ServerAddress);
-            csp_log_info("reboot system request sent to address: %u", m_ServerAddress);
-
-            /* Send data packet (string) to server */
-
-            /* 1. Connect to host on 'server_address', port MY_SERVER_PORT with regular UDP-like protocol and 1000 ms timeout */
-            csp_conn_t * conn = csp_connect(CSP_PRIO_NORM, m_ServerAddress, MY_SERVER_PORT, 1000, CSP_O_NONE);
-            if (conn == NULL) {
-                /* Connect failed */
-                csp_log_error("Connection failed");
-                return CSP_TASK_RETURN;
-            }
-
-            /* 2. Get packet buffer for message/data */
-            csp_packet_t * packet = static_cast<csp_packet_t*>(csp_buffer_get(100));
-            if (packet == NULL) {
-                /* Could not get buffer element */
-                csp_log_error("Failed to get CSP buffer");
-                return CSP_TASK_RETURN;
-            }
-
-            /* 3. Copy data to packet */
-            snprintf((char *) packet->data, csp_buffer_data_size(), "Hello World (%u)", ++count);
-
-            /* 4. Set packet length */
-            packet->length = (strlen((char *) packet->data) + 1); /* include the 0 termination */
-
-            /* 5. Send packet */
-            if (!csp_send(conn, packet, 1000)) {
-                /* Send failed */
-                csp_log_error("Send failed");
-                csp_buffer_free(packet);
-            }
-
-            /* 6. Close connection */
-            csp_close(conn);
-        }
-
-        return CSP_TASK_RETURN;
-    }
-/* End of client task */
-
     void ComponentCspDriver::Init() {
         // initialize conf information
         uint8_t address = 1;
@@ -178,16 +68,16 @@ namespace kronos {
         csp_route_print_table();
 
         /* Start server thread */
-        if ((m_ServerAddress == 255) ||  /* no server address specified, I must be server */
-            (default_iface == NULL)) {  /* no interfaces specified -> run server & client via loopback */
-            csp_thread_create(task_server, "SERVER", 1000, NULL, 0, NULL);
-        }
+//        if ((m_ServerAddress == 255) ||  /* no server address specified, I must be server */
+//            (default_iface == NULL)) {  /* no interfaces specified -> run server & client via loopback */
+//            csp_thread_create(task_server, "SERVER", 1000, NULL, 0, NULL);
+//        }
 
         /* Start client thread */
-        if ((m_ServerAddress != 255) ||  /* server address specified, I must be client */
-            (default_iface == NULL)) {  /* no interfaces specified -> run server & client via loopback */
-            csp_thread_create(task_client, "CLIENT", 1000, NULL, 0, NULL);
-        }
+//        if ((m_ServerAddress != 255) ||  /* server address specified, I must be client */
+//            (default_iface == NULL)) {  /* no interfaces specified -> run server & client via loopback */
+//            csp_thread_create(task_client, "CLIENT", 1000, NULL, 0, NULL);
+//        }
 
     }
 
