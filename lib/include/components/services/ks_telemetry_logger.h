ccfaf0a4ecb4bd306c46a6b4745a7695fa8d4af7
Thomas Pedneault <thomas@pedneault.dev>
2021-11-21 17:13:38 -0500

    Logging telemetry into file

--- lib/include/components/services/ks_telemetry_logger.h
+++ lib/include/components/services/ks_telemetry_logger.h
@@ -1,29 +1,37 @@
 #pragma once
 
 #include "ks_component_active.h"
-
 #include "ks_vector.h"
+#include "ks_apollo_format.h"
+#include "ks_file_manager.h"
 
 namespace kronos {
 
     struct TelemetryChannel {
         String name = "";
-        uint32_t (*retrieveTelemetry)() = nullptr;
+        uint32_t (* retrieveTelemetry)() = nullptr;
     };
 
     struct TelemetryRateGroup {
+         ~TelemetryRateGroup() {
+             delete apolloExporter;
+         }
+
+        String name = "";
         uint32_t tickCount = 0;
         uint32_t tickRate;
         Vector<TelemetryChannel> channels;
+        ApolloExporter* apolloExporter;
     };
 
     class ComponentTelemetryLogger : ComponentActive {
     public:
         KsCmdResult ProcessEvent(const EventMessage& message) override;
 
-        KsResult AddTelemetryGroup(uint32_t rate, const Vector<TelemetryChannel>& channels);
+        KsResult AddTelemetryGroup(const String& name, uint32_t rate, const Vector<TelemetryChannel>& channels);
     private:
         Vector<TelemetryRateGroup> m_TelemetryRateGroups;
+        ComponentFileManager* m_FileManager = nullptr;
     };
 
 }
--- lib/src/components/services/ks_telemetry_logger.cpp
+++ lib/src/components/services/ks_telemetry_logger.cpp
@@ -3,13 +3,21 @@
 namespace kronos {
 
     KsCmdResult ComponentTelemetryLogger::ProcessEvent(const EventMessage& message) {
-        switch(message.eventCode) {
+        switch (message.eventCode) {
             case KS_EVENT_CODE_RATE_GROUP_TICK:
-                for(TelemetryRateGroup i_RateGroup : m_TelemetryRateGroups) {
+                for (TelemetryRateGroup i_RateGroup: m_TelemetryRateGroups) {
                     i_RateGroup.tickCount++;
                     if (i_RateGroup.tickCount >= i_RateGroup.tickRate) {
                         i_RateGroup.tickCount = 0;
-                        // TODO: Log Telemetry
+
+                        // Get the values from the telemetry into a vector.
+                        Vector<uint32_t> telemetryData;
+                        for(TelemetryChannel i_Channel : i_RateGroup.channels) {
+                            telemetryData.Add(i_Channel.retrieveTelemetry());
+                        }
+
+                        // Write the vector to the ApolloExporter.
+                        i_RateGroup.apolloExporter->WriteRow(telemetryData);
                     }
                 }
                 break;
@@ -17,11 +25,27 @@ namespace kronos {
         return ComponentActive::ProcessEvent(message);
     }
 
-    KsResult ComponentTelemetryLogger::AddTelemetryGroup(uint32_t rate, const Vector<TelemetryChannel>& channels) {
+    KsResult ComponentTelemetryLogger::AddTelemetryGroup(const String& name, uint32_t rate, const Vector<TelemetryChannel>& channels) {
+        if(m_FileManager == nullptr) {
+            m_FileManager = new ComponentFileManager("File Manager", "C:");
+        }
+
         TelemetryRateGroup newRateGroup;
+        newRateGroup.name = name;
         newRateGroup.tickCount = 0;
         newRateGroup.tickRate = rate;
         newRateGroup.channels = channels;
+
+        // Initialize the file for logging this telemetry group.
+        File* file = m_FileManager->Open("/" + name + ".txt", KS_OPEN_MODE_CREATE | KS_OPEN_MODE_WRITE_ONLY);
+
+        Vector<ApolloHeader> headers;
+        for(size_t i = 0; i < channels.Size(); i++) {
+            headers[i].name = channels[i].name;
+            headers[i].dataType = KS_APOLLO_FLOAT;
+        }
+
+        newRateGroup.apolloExporter = new ApolloExporter(file, headers);
 
         m_TelemetryRateGroups.Add(newRateGroup);
 
