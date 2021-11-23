1b5b2b3a8776a9b8e9c893c0eb10f5a212ad122a
Thomas Pedneault <thomas@pedneault.dev>
2021-11-23 13:20:00 -0500

    made index.md mainpage

--- docs/html/search/pages_0.js
+++ docs/html/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['spacecraft_20framework_330',['Spacecraft Framework',['../md__home_thomas_Repos_orthus_fsw_kronos_lib_include_index.html',1,'']]]
+  ['spacecraft_20framework_330',['Spacecraft Framework',['../index.html',1,'']]]
 ];
